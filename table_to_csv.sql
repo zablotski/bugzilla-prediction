cbe95869d50afc2b87d9f7b2790d31c0dcdd7146
Yauheni Zablotski <e.zablotski@gmail.com>
2014-11-18 23:29:38 +0100

    changed sql file

--- table_to_csv.sql
+++ table_to_csv.sql
@@ -1,6 +1,6 @@
-SELECT *
+SELECT `creation_time`, `summary`,  `last_change_time`,  `platform`,  `op_sys`,  `priority`,  `depends`,  `blocks`,  `component`
 FROM bug
-INTO OUTFILE '~/tmp/bugs.csv'
+INTO OUTFILE '/tmp/bugs_v1.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
