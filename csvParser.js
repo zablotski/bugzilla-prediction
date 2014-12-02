fb36948948f01306ff24b7d0dba407a2c9d59bef
Yauheni Zablotski <e.zablotski@gmail.com>
2014-12-02 22:20:49 +0100

    working csvParser

--- csvParser.js
+++ csvParser.js
@@ -2,7 +2,8 @@ var fs = require('fs'); //io
 var parse = require('csv-parse'); //parse csv from file
 var bz = require("bz"); //bugzilla api library
 var stringify = require('csv-stringify'); //js objects to csv text
-
+var moment = require('moment'); //library for comparision dates
+var start = Date.now();//timer =)
 
 var bugzilla = bz.createClient({
   username: '333onk@gmail.com',
@@ -30,20 +31,38 @@ function writeFile(filename, text){
 
 function iterate(data){
 
-    data[0].push('ASSIGNED', 'RESOLVED', 'COUNT_SUMMARY', 'COUNT_BLOCKS', 'COUNT_DEPENDS');
+    data[0].push('ASSIGNED', 'RESOLVED', 'HOURS', 'COUNT_SUMMARY', 'COUNT_BLOCKS', 'COUNT_DEPENDS', 'EVENTS_COUNT', 'HOURS_BEFORE_ASSIGNED');
 
     console.log('length: ' + data.length + '-> '+JSON.stringify(data[0]));
 
 
     var i=1;
     function repeater(i) {
+        var ASSIGNED, RESOLVED, SUMMARY ;
         if( i < data.length ) {
-            bugzilla.bugHistory(parseInt(data[i][0]), function(error, history) {
-                if (!error && getAssignedTimeFromBugHistory(history) && getResolvedTimeFromBugHistory(history)) {
-                    data[i].push(getAssignedTimeFromBugHistory(history), getResolvedTimeFromBugHistory(history), data[i][3].split(' ').length.toString(), (data[i][8].split(',').length+1).toString(), (data[i][6].split(',').length+1).toString());
-                    console.log(i + "-> " + JSON.stringify(data[i]));
-                }
-                repeater(i+1);
+            bugzilla.getBug(parseInt(data[i][0]), function(error, bug) {
+                bugzilla.bugHistory(parseInt(data[i][0]), function(error, history) {
+                    if (!error && getAssignedTimeFromBugHistory(history) && getResolvedTimeFromBugHistory(history)) {
+
+                        SUMMARY = bug.summary.replace(/"/g, "").replace(/'/g, "");
+                        ASSIGNED = getAssignedTimeFromBugHistory(history);
+                        RESOLVED = getResolvedTimeFromBugHistory(history);
+
+                        data[i].push(
+                            ASSIGNED, 
+                            RESOLVED, 
+                            moment(RESOLVED).diff(moment(ASSIGNED), 'hours'), 
+                            SUMMARY.split(' ').length.toString(), 
+                            bug.blocks.length, 
+                            bug.depends_on.length, 
+                            getEventsCountInBugHistory(history),
+                            moment(ASSIGNED).diff(moment(bug.creation_time), 'hours')
+
+                        );
+                        console.log(i + "-> " + JSON.stringify(data[i]));
+                    }
+                    repeater(i+1);
+                });
             });
         }
         if(i == data.length){
@@ -73,6 +92,19 @@ function getAssignedTimeFromBugHistory(bugHistory){
     return false;
 }
 
+function getEventsCountInBugHistory(bugHistory){
+    var i;
+    for (i = 0; i< bugHistory[0].history.length; i++) {
+
+        if( bugHistory[0].history[i].changes.filter(function(ch){
+           return  ch.added == "ASSIGNED";
+       }).length){
+            break;
+        }
+    }
+    return i+1;
+} 
+
 
 function getResolvedTimeFromBugHistory(bugHistory){
 
@@ -89,3 +121,8 @@ function getResolvedTimeFromBugHistory(bugHistory){
     }
     return false;
 }
+
+process.on("exit", function() {
+  var end = Date.now();
+  console.log("Time taken: %ds", (end - start)/1000);
+});
